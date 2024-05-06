264e53729b4f59d11dcd4fe37968f689fd590e84
Shubham91816 <149255677+Shubham91816@users.noreply.github.com>
2024-05-06 09:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
